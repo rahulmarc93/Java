1a6411ad7a980a8966fa5d24000ad49a55e0d548
rahulmarc93 <148323098+rahulmarc93@users.noreply.github.com>
2024-10-09 12:50:21 +0530

    Add files via upload
    Data types with small example in the code itself.
    Thank you !

---
+++ Helloworld_$.java
@@ -0,0 +1,25 @@
+package games_1;
+
+public class Helloworld_$ {
+
+	public static void main(String[] args) {
+		byte num1 = 10;
+		short num2 = 20;
+		int num3 = 30;
+		long num4 = 404040404040404040L;
+		char letter = 'A';
+		float num5 = 50.23F;
+		double num6 = 60.86;
+		boolean torf = true;
+		
+		System.out.println(num1);
+		System.out.println(num2);
+		System.out.println(num3);
+		System.out.println(num4);
+		System.out.println(letter);
+		System.out.println(num5);
+		System.out.println(num6);
+		System.out.println(torf);
+	}
+
+}
