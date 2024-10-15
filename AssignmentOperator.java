4a93eacabcf2893bcc0c6f58b60b70855d715adf
rahulmarc93 <148323098+rahulmarc93@users.noreply.github.com>
2024-10-15 13:44:19 +0530

    Add files via upload

---
+++ AssignmentOperator.java
@@ -0,0 +1,15 @@
+package Operators;
+
+public class AssignmentOperator {
+
+	public static void main(String[] args) {
+				int x=12;
+				System.out.println(x);
+				int y=24;
+				System.out.println(y);
+				y=x;
+				System.out.println(y);
+
+	}
+
+}
