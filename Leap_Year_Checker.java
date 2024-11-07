9a89c5480aef31a4aae942dc31cf563daa0ec781
rahulmarc93 <148323098+rahulmarc93@users.noreply.github.com>
2024-11-07 14:27:09 +0530

    Add files via upload

---
+++ Leap_Year_Checker.java
@@ -0,0 +1,20 @@
+package interviewCodes;
+
+import java.util.Scanner;
+public class Leap_Year_Checker {
+
+	    public static void main(String[] args) {
+	        Scanner scanner = new Scanner(System.in);
+	        System.out.print("Enter a year: ");
+	        int year = scanner.nextInt();
+
+	        // Check if the year is a leap year
+	        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+	            System.out.println(year + " is a leap year.");
+	        } else {
+	            System.out.println(year + " is not a leap year.");
+	        }
+
+	        scanner.close();
+	    }
+}
